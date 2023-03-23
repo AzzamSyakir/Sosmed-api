818ed5f3d70be1db9ec5ee6d47a5f9a14cc91149
Asa <94348484+AzzamSyakir@users.noreply.github.com>
2023-03-23 11:36:04 +0700

    delete yng g penting

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
+php 
 // Auth 
 Route::post('authenticate', [AuthController::class, 'authenticate']);
 Route::post('login', [AuthController::class, 'LoginUser']);
